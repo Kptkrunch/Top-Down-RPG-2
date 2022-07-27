33d004c7b02d3769a992ee16fad1cfc7464c18bc
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-07-26 19:43:22 -0500

    Follow camera and confiner completed, new prefabs

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour {
+
+	public GameObject player;
+	void Awake() {
+		if (!PlayerController.Instance) {
+			Instantiate(player);
+		}
+	}
+}
+
